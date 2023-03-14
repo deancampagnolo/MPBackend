a90094ae8cc2dad262d5090cd951750dd107ab18
Dean Campagnolo <deancampagnolo@gmail.com>
2023-03-13 17:34:09 -0700

    Changed cross origin allowed url

--- src/main/kotlin/com/example/mpbackend/CrossOriginPreset.kt
+++ src/main/kotlin/com/example/mpbackend/CrossOriginPreset.kt
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.CrossOrigin
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
-@CrossOrigin(origins = ["http://localhost:8080", "https://localhost:8080", "https://masterpiecemusic.io", "https://famous-mandazi-31dc6a.netlify.app/", "https://main--famous-mandazi-31dc6a.netlify.app/"])
+@CrossOrigin(origins = ["http://localhost:3000", "https://localhost:3000", "https://masterpiecemusic.io", "https://famous-mandazi-31dc6a.netlify.app/", "https://main--famous-mandazi-31dc6a.netlify.app/"])
 annotation class CrossOriginPreset
 
